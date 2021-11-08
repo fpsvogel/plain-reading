62fdb11183af4d779c8417df7dd4b38e9a31de3d
Felipe Vogel <fps.vogel@gmail.com>
2021-11-08 15:26:57 -0500

    partial move to esbuild

--- Gemfile
+++ Gemfile
@@ -13,7 +13,10 @@ gem "pg", "~> 1.1"
 gem "puma", "~> 5.0"
 
 # Use JavaScript with ESM import maps [https://github.com/rails/importmap-rails]
-gem "importmap-rails", ">= 0.3.4"
+# gem "importmap-rails", ">= 0.3.4"
+
+# Use jsbundling [https://github.com/rails/jsbundling-rails]
+gem 'jsbundling-rails'
 
 # Hotwire's SPA-like page accelerator [https://turbo.hotwired.dev]
 gem "turbo-rails", ">= 0.7.11"
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
-import "controllers"
+import "./controllers"
 
 import { setBasePath } from '@shoelace-style/shoelace'
 setBasePath("https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.57/dist/")
@@ -13,3 +13,4 @@ setBasePath("https://unpkg.com/@shoelace-style/shoelace@2.0.0-beta.57/dist/")
 // Promise.all(tagsUsed.map(tag => customElements.whenDefined(tag))).then(() => {
 //   // All components have loaded now, transition in and show the UI here
 // });
+
