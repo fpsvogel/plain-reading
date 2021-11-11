05426dea7962264d2e5c1a9d3a6fb4f2adbf3161
Felipe Vogel <fps.vogel@gmail.com>
2021-11-11 10:53:09 -0500

    delete auto-generated tests

--- test/models/csv_config_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class CsvConfigTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/format_category_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class FormatCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
