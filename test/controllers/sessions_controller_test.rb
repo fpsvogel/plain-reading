a2e02cfa3cf74925583b30d956c2e32ad5b6e0af
Felipe Vogel <fps.vogel@gmail.com>
2021-12-03 22:07:30 -0500

    add create to SessionsController test

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,11 +5,18 @@ class SessionsControllerTest < ActionController::TestCase
   # https://github.com/NoamB/sorcery-example-app/blob/master/test/functional/user_sessions_controller_test.rb
 
   def setup
-    @user = users(:felipe)
+    @user = users(:sam)
   end
   test "should get new" do
     get :new
     assert_response :success
+  end
+
+  test "should get create" do
+    post :create, params: { email: "sam@example.com",
+                            password: "password" }
+    assert_redirected_to root_path
+    assert_equal("Logged in successfully.", flash[:notice])
   end
 
   test "should delete destroy" do
